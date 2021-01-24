2102fa4099f4f847e9c2e7b584b7dfde52bcc5cc
Sebastian Sikora <ssikora80@gmail.com>
2021-01-24 15:07:30 +0100

    Add main structure function generateTags, change index.html

--- js/script.js
+++ js/script.js
@@ -52,7 +52,8 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optArticleAuthorSelector = '.post-author';
+  optArticleAuthorSelector = '.post-author',
+  optTagsListSelector = '.tags.list';
 
 function generateTitleLinks(customSelector = '') {
 
@@ -148,7 +149,6 @@ function generateTags() {
     /* [DONE?] insert HTML of all the links into the tags wrapper */
 
     tagsWrapper.innerHTML = html;  // ????? czemu brak odstępów między tagami pod artykułem???????
-
 
   /* [DONE] END LOOP: for every article: */
   }
@@ -349,3 +349,43 @@ function addClickListenersToAuthors () {
 
 addClickListenersToAuthors ();
 
+function generateTags(){
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
+
+  /* find all articles */
+
+  /* START LOOP: for every article: */
+
+    /* find tags wrapper */
+
+    /* make html variable with empty string */
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+      /* [NEW] check if this link is NOT already in allTags */
+      if(allTags.indexOf(linkHTML) == -1){
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector('.tags');
+
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(' ');
+}
